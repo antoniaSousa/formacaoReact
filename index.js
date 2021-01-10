f459268b2a9e1c06e2f8d106ea5888edc381ee9f
antonia <antonia@unifesspa.edu.br>
2021-01-10 16:51:49 -0300

    modulo JavaScript

--- index.js
+++ index.js
@@ -1,45 +1,23 @@
-
-class Cliente {
-    nome;
-    cpf;
-
-}
-class ContaCorrente {
-    agencia;
-    _saldo = 0;
-
-    sacar(valor){
-        if(this._saldo >= valor){
-            this._saldo -= valor;
-            return valor;
-        }
-    }
-
-    depositar(valor){
-        if(valor <= 0){
-            return;
-        }
-        this._saldo += valor;
-    }
-}
+import {Cliente} from "./Cliente.js";
+import {ContaCorrente} from "./ContaCorrente.js";
 const cliente1 = new Cliente();
 
- cliente1.nome = "Ricardo";
- cliente1.cpf = 11122233309;
+cliente1.nome = "Ricardo";
+cliente1.cpf = 11122233309;
 
 const cliente2 = new Cliente();
 cliente2.nome = "Alice";
 cliente2.cpf = 88822233309;
 
 
- const contaCorrenteRicardo = new ContaCorrente();
- contaCorrenteRicardo.agencia = 1001;
+const contaCorrenteRicardo = new ContaCorrente();
+contaCorrenteRicardo.agencia = 1001;
 
- contaCorrenteRicardo.depositar(40);
- contaCorrenteRicardo.depositar(100);
- contaCorrenteRicardo.depositar(100);
+contaCorrenteRicardo.depositar(40);
+contaCorrenteRicardo.depositar(100);
+contaCorrenteRicardo.depositar(100);
 
- const valorSacado = contaCorrenteRicardo.sacar(50);
- console.log(valorSacado);
- 
- console.log(contaCorrenteRicardo);
+const valorSacado = contaCorrenteRicardo.sacar(50);
+console.log(valorSacado);
+
+console.log(contaCorrenteRicardo);
